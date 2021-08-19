9aaa6929b08d219138c14a5a8084d9f7abd24ae3
calfar <calumfarrar@gmail.com>
2021-08-19 14:53:43 +1000

    End of Day Two

--- Python Carpentry Day 2_1.py
+++ Python Carpentry Day 2_1.py
@@ -156,14 +156,55 @@ if masses[1] > 3.0 and masses[2] <=10 or velocity[0] == 54:
 
 #e.g. velocity[0] = 54 #this would reassign the value of value 0 to 54 in the velocity list
 
+## processing small files
+import glob
+import pandas as pd
+for filename in glob.glob('/Users/Calum/Desktop/python-novice-gapminder-data/data/*.csv'):
+    cont = pd.read_csv(filename)
+    if len(cont) <50:
+        print(filename, len(cont))
+# the * in 'data/*.csv) is a wildcard, it will read any csv file
+# the if len(cont) <50 means that the only files displayed are the ones with less than 50 rows?
 
+### Creating Functions ###
+#def for defining a function
+def print_gretting():
+    print("Hello World")
+print_gretting()    
 
+#Example of setting parameters on printing the date
+#doesn't like leading zeroes e.g. 08
+def print_date(year, month, day):
+    joined = str(year) + '-' + str(month) + '-' + str(day)
+    if month == [4, 6, 9, 11] and day > 31:
+        print("wrong date input")
+    elif month == 2 and day > 28:
+        print("wrong date input")
+    elif year > 2200 or month > 12 or day > 31:
+        print("wrong date input")
+    else:
+        print(joined)
 
+print_date(2021, 2, 29) 
 
-    
-    
+# averages #
+# return is what the function will actually return back to you if conditions are met
 
+def average(values):
+    if len(values) == 0:
+        print("You must provide some values")
+        return None
+    return sum(values) / len(values) #this is for the mean
 
+def report(pressure):
+    print('pressure is', pressure)
+    return(pressure)
+
+print('calling' , report(22.5))
+
+r = report(22.5) #will always auto print from report function
+r # running the variable by itself does not run the function, just the value
+# function must be defined prior to using it in a script
 
 
 
