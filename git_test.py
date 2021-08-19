7e4c17eb8bad7f3fcc1ab132ab763318f98db77b
calfar <calumfarrar@gmail.com>
2021-08-19 10:36:44 +1000

    added git commit comments to gittest

--- git_test.py
+++ git_test.py
@@ -6,3 +6,18 @@ Created on Thu Aug 19 09:45:22 2021
 """
 
 ## Git Test ##
+#Open git-bash
+#cd <file direction> seems to not like spaces
+# use pwd to check directory
+#git init to create the git repository in the file location
+#ls -a to view secret git foldre
+#create external git repository e.g. github or gitlab
+#git remote add origin <link to external repository> e.g. https://github.com/user/project
+#git remote -v to check location
+#git add . to add all files at location
+#git commit -m 'comment' the comment shows up on external repository as a note
+#git push origin master # this pushes the local to the external
+# You will be asked for login details, generate a token via github if at github
+#put in username e.g. calfar
+#then put the token in to the password dialouge
+#this should then upload and appear on the external repository
